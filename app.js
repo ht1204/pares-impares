5dbd0202c4b1888e776d6b4dd9a7f52a481758b9
Hector Triana <ptb1204@hotmail.com>
2022-10-16 20:49:48 -0500

    refactoring approach SSR odd-even html tag  paragraphs

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
+const { htmlGenerator } = require("./utils");
 const app = express();
 
 const PORT = 3000;
 
-app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  const num = "Soy ";
-  res.render("index", { num });
+    const index = htmlGenerator();
+    res.status(202).send(index);
 });
 
 app.listen(PORT, () => {
---
+++ utils/index.js
@@ -0,0 +1,38 @@
+
+const paragraphGenerator = () => {
+    let paragraphs = '';
+
+    for (let i = 1; i < 50; i++) {
+        paragraphs += '<p>'+i+' Soy ';
+        if (i % 2 === 0) paragraphs += 'Par!</p>';
+        else paragraphs += 'Impar!</p>';
+    }
+
+    return paragraphs;
+
+}
+
+
+const htmlGenerator = () => {
+    const paragraphs = paragraphGenerator();
+    const index = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Pares e Impares</title>
+        </head>
+        <body>
+            ${paragraphs}
+        </body>
+        </html>
+    `;
+
+    return index;
+
+};
+
+module.exports = {
+    htmlGenerator,
+}
